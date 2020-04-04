51e1acf8f977120303a5c740f4210c5b5b1fd5f7
Fahad <fahadadnan123@gmail.com>
2020-04-04 16:00:45 -0400

    first commit

--- background_new.js
+++ background_new.js
@@ -1,13 +1,17 @@
-//When installed the extension will have an available popup on pages containing: youtube.com
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-        
-        new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: '.youtube.com'},
-      })],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+var commentstring = "comments";
+var redditstring = "https://www.reddit.com"
+var stringarr = ["top", "best", "hot", "new", "rising"];
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  var urlstring = changeInfo.url;
+  console.log("The URL just changed B");
+  console.log("The Old URL was " + urlstring);
+  console.log("The tab is.." + tab);
+  console.log("The tab ID is.." + tabId);
+
+ if (typeof(changeInfo.url) !== 'undefined' && !(urlstring.includes(commentstring) && urlstring.includes(redditstring))) {
+    console.log("restarting scripts");
+    chrome.tabs.executeScript({file: 'content.js'});
+  }
+
 });
--- content.js
+++ content.js
@@ -1,83 +1,117 @@
 
 console.log("page has been opened");
 
-//Creating targetnode/value to check for download link + what values to observe
-const targetnode = document.getElementsByTagName("a")[5];
-const config = { attributes: true, childList: false, subtree: false };
+// NOTE: typically nodes are const, but the page dynamically updates, so thye may change=>var. 
+// nodes being observed on. 
+var target_main_feed = document.getElementsByClassName("rpBJOHq2PR60pnwJlUyP0")[0];
+var target_clicked_post = document.getElementsByTagName("BODY")[0];
 
-// Callback function to execute when mutations are observed
-const callback = function(mutationsList, observer) {
-    for(let mutation of mutationsList) {
-        console.log ("Checking For mutations with href download link");
-        if (mutation.type === 'attributes') { //mutation has been observed
-            console.log('The ' + mutation.attributeName + ' attribute was modified, download link generated ');
-             document.getElementsByTagName('a')[5].click();
-             observer.disconnect();
-             
-            chrome.storage.sync.set({previous_tab: false}, function() {
-                console.log("the previous tab is no longer utube");
-            });
-            chrome.storage.sync.set({mp3btnclick: false}, function() {
-                console.log("no previous button clicks stored. Thanks for using my app - Fahad Adnan");
-            });
-            setTimeout(function() {
-                window.close();
-              }, 1500);
-            
+//configuration of properties being observed.
+var config_main_feed = { attributes: false, childList: true, subtree: false };
+var config_clicked_post = { attributes: true, childList: false, subtree: false };
+
+//start index of what post is being checked if its a promoted post.
+var start =0;
+
+
+
+function DeletePromotedPosts(start,end){
+    var postDiv, classNamePost, lenClass, nodechild;
+    console.log("The Start Value is " + start + "the end value is: " + end);
+
+    for( i= start; i < end; ++i){
+         postDiv = target_main_feed.childNodes[i];
+        nodechild = target_main_feed.childNodes[i].childNodes[0].childNodes[0];
+         if(typeof(nodecihld) !== 'undefined' && nodechlid != null){
+             console.log("The element of: " + i + "does not exist");
+             break;
+         }
+         if(typeof(target_main_feed.childNodes[i].childNodes[0].childNodes[0]) !== 'undefined'){
+            classNamePost = target_main_feed.childNodes[i].childNodes[0].childNodes[0].className;
+            lenClass = classNamePost.length;
+            if ((classNamePost.substring(lenClass-12, lenClass)) == "promotedlink"){
+                postDiv.remove();
+                console.log("the post item: " + i + "is promoted and has been removed");
+                --end;
+            }
+    }
+}
+}
+
+function RemoveAds(){
+    var adsremoved = 0;
+    var ads;
+    setTimeout(function() { // wait post tab to open and all the posts to load. 
+        ads = document.getElementsByClassName("_1rmObrmUCfC5t42SbwkzYC");
+        if(typeof(ads) !== 'undefined'){
+            while(ads.length !=0){
+                console.log(ads.length + "This is JUSTICE");
+                if(typeof(ads[0]) !== 'undefined'){
+                    ads[0].remove();
+                    console.log("A ad was removed!");
+                    adsremoved=1;
+                }
+            }
+        }
+    }, 500);
+}
+
+var callback_clicked_post = function(mutationsList, observer_clicked_post) {
+    for(let mutation of mutationsList) {  
+        if (mutation.type === 'attributes') {
+            console.log("Clicked on a post");
+            setTimeout(function() { // wait for page to load multiple the posts 
+            RemoveAds();
+             }, 300);
         }
     }
 };
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
 
-
-
-//Check whether any code should be executed on this page. 
-chrome.storage.sync.get('previous_tab', function(should_execute) {
-    if(should_execute.previous_tab){
-        chrome.storage.sync.get('mp3btnclick', function(data) {
-            if (data.mp3btnclick){ 
-                GenerateDownloadLink();
-            }
-            else {
-                document.getElementById("mp4").click();
-                GenerateDownloadLink();
-            }
-        });
+var callback_main = function(mutationsList, observer_main_feed) {
+    for(let mutation of mutationsList) {  
+        if (mutation.type === 'childList') {
+            console.log(target_main_feed.childElementCount)
+            setTimeout(function() { // wait for page to load multiple the posts 
+                if(target_main_feed.childElementCount > start){
+                    var numchild = target_main_feed.childElementCount;
+                    observer_main_feed.disconnect();
+                    DeletePromotedPosts(start, numchild);
+                    start = numchild;
+                    observer_main_feed.observe(target_main_feed, config_main_feed);
+                }
+              }, 300);
+        }
     }
-});
+};
 
 
-//Generates a download link for a video which is observed until the link is ready.
-function GenerateDownloadLink (){ 
+//creating mutation observers for each node.
+var observer_clicked_post = new MutationObserver(callback_clicked_post);
+var observer_main_feed = new MutationObserver(callback_main);
 
-    chrome.storage.sync.get('utubelink', function(data) {
-        document.getElementById("input").value = data.utubelink; 
-        console.log ("URL Has been entered into search bar"); 
-        document.getElementById('submit').click();
-        console.log ("confirm - generate download-link button has been clicked");
-        // Start observing the target node for configured mutations
-        observer.observe(targetnode, config);
-       });
+
+// check if nodes are undefined and if not observe.
+function observe_nodes() {
+
+    if(typeof(target_clicked_post) !== 'undefined'){
+        observer_clicked_post.observe(target_clicked_post, config_clicked_post);
     }
-    
+    if(typeof(target_main_feed) !== 'undefined'){
+        observer_main_feed.observe(target_main_feed, config_main_feed);
+    }
+}
 
 
 
+// When the page updates dynamically, the window don't reload-  add this do what typically happens on a window reload
+RemoveAds();
+observe_nodes();
 
 
+window.addEventListener('load', 
+  function() { 
+    console.log("removing the initial ads.");
+    observe_nodes();
+    RemoveAds();
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Later, you can stop observing
-//observer.disconnect();
+  }, false);
